d43bad11e618ef9774ffbf845dbbc187d8408a1b
shanu95 <shannirmala95@gmail.com>
2018-05-23 11:29:59 +0530

    commit4

---
+++ app/src/main/java/com/gpacalc/shannirmala/gpacalculator/student_homepage.java
@@ -0,0 +1,13 @@
+package com.gpacalc.shannirmala.gpacalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class student_homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_homepage);
+    }
+}
