518b16cd1e5fe42cf7c1d79a595d8e8d928901f8
shanu95 <shannirmala95@gmail.com>
2018-05-28 11:37:12 +0530

    commit7

---
+++ app/src/main/java/com/gpacalc/shannirmala/gpacalculator/add_subject.java
@@ -0,0 +1,13 @@
+package com.gpacalc.shannirmala.gpacalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_subject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subject);
+    }
+}
